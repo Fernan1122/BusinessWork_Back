73dbf973e5b547d01d0e0664aa9844ad9511d9d7
Luis Fernando Arciniegas <you@example.com>
2020-12-20 20:41:07 -0500

    Backend equipo 5

---
+++ schemas.py
@@ -0,0 +1,60 @@
+from datetime import date
+from os import name
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+'''
+class DocumentsIn(BaseModel):
+    id: int
+    owner_id: int
+'''
+
+class DocumentsBase(BaseModel):
+    name_doc: str
+    exp: date
+    notif: int
+    descrip: str
+
+
+class DocumentsCreate(DocumentsBase):
+    pass
+
+
+class Documents(DocumentsBase):
+    id: int
+    name_doc: str
+    exp: date
+    notif: int
+    descrip: str
+    owner_username: str
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    name: str
+    last_name: str
+    celular: str
+
+class UserIn(BaseModel):
+    username    : str
+    password    : str
+
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    username: str
+    email: str
+    name: str
+    last_name: str
+    celular: str
+    user_password: str
+    documents: List[Documents] = []
+
+    class Config:
+        orm_mode = True
